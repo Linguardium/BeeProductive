8ef74d1ab8876c723feaeafe500454147facc96b
Linguardium <61733861+Linguardium@users.noreply.github.com>
2020-03-11 21:44:18 -0700

    attempt to check loot context rather than id path
    checking path only to determine if its a village chest or not.

--- src/main/java/io/github/alloffabric/beeproductive/init/BeeProdLoot.java
+++ src/main/java/io/github/alloffabric/beeproductive/init/BeeProdLoot.java
@@ -2,6 +2,7 @@ package io.github.alloffabric.beeproductive.init;
 
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.loot.UniformLootTableRange;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
@@ -10,7 +11,7 @@ public class BeeProdLoot {
 
 	public static void init() {
 		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
-			if (id.getPath().contains("chests") && !id.getPath().contains("village")) {
+			if (supplier.getType.equals(LootContextTypes.CHEST) && !id.getPath().contains("village")) {
 				FabricLootPoolBuilder builder = FabricLootPoolBuilder.builder();
 				builder.withRolls(UniformLootTableRange.between(0, 5));
 				builder.withCondition(RandomChanceLootCondition.builder(0.5f));
